8da5d76b7619c678c5084560a37dae1d8022e740
Md Farhatus Sium <252-35-001@diu.edu.bd>
2025-12-09 23:06:01 +0600

    Implement row and column swap functions and add main logic for matrix manipulation

--- I.c
+++ I.c
@@ -1,19 +1,41 @@
 #include <stdio.h>
-void nTimes(int n,char c){
-    for(int i=1;i<=n;i++){
-        printf("%c ",c);
+ void rSwap(int n,int x,int y,int a[][500]){
+         for (int i = 0; i < n; i++) {
+        int temp = a[x][i];
+        a[x][i] = a[y][i];
+        a[y][i] = temp;
+       }
     }
-}
-int main(){
-    int t;
-    scanf("%d",&t);
-    while(t--){
-    int n;
-    char c;
-    scanf("%d %c", &n, &c);
-    nTimes(n,c);
+void cSwap(int n,int x,int y,int a[][500]){
+         for (int i = 0; i < n; i++) {
+        int temp = a[i][x];
+        a[i][x] = a[i][y];
+        a[i][y] = temp;
+       }
+    }
+
+int main() {
+   int n,x,y;
+    scanf("%d %d %d", &n, &x, &y);
+    x--;
+    y--;
+    int a[500][500];
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            scanf("%d", &a[i][j]);
+        }
+    }
+            
+    rSwap(n,x,y,a);
+    cSwap(n,x,y,a);
+    
+    for (int i = 0; i < n; i++){
+        for (int j = 0; j < n; j++){
+            printf("%d ", a[i][j]);
+        }
     printf("\n");
     }
     
+
     return 0;
 }
